fdef75d36a2e560d941378dfe6279a40ffb32af7
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2021-12-02 00:19:51 -0300

    [develop] add basic cli support

---
+++ src/cli.rs
@@ -0,0 +1,49 @@
+use clap::{App as ClapApp, Arg};
+
+use super::banner::BANNER;
+
+/// nmkill CLI
+pub struct Cli {
+    /// time in ms between two ticks.
+    pub path: String,
+    /// time in ms between two network calls.
+    pub criteria: String,
+    /// whether unicode symbols are used to improve the overall look of the app
+    pub enhanced_graphics: bool,
+}
+
+impl Cli {
+    pub fn new() -> Cli {
+        Cli {
+            path: "".to_string(),
+            criteria: "node_modules".into(),
+            enhanced_graphics: true,
+        }
+    }
+
+    /// create a new clapapp instance
+    pub fn get_clap_app<'a, 'b>(&mut self) -> ClapApp<'a, 'b> {
+        ClapApp::new(env!("CARGO_PKG_NAME"))
+            .version(env!("CARGO_PKG_VERSION"))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about(env!("CARGO_PKG_DESCRIPTION"))
+            .usage("Press `?` while running the app to see keybindings")
+            .before_help(BANNER)
+            .arg(
+                Arg::with_name("path")
+                    .short("p")
+                    .long("path")
+                    .required(true)
+                    .help("Set the path to scan for the criteria.")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("criteria")
+                    .short("c")
+                    .long("criteria")
+                    .help("Set the criteria to search.")
+                    .default_value("node_modules")
+                    .takes_value(true),
+            )
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,56 @@
+use rayon::prelude::*;
 use std::fs;
 use std::path::PathBuf;
 
+use glob::glob;
+
 pub fn find_directories(path: &str, criteria: &str) -> Vec<PathBuf> {
-    let mut directories = vec![];
-    // search recursively for directories "node_modules" and push them to the vector
-    for entry in fs::read_dir(path).unwrap() {
-        let entry = entry.unwrap();
-        if entry.file_type().unwrap().is_dir() {
-            let path = entry.path();
-            if path.ends_with(criteria) {
-                directories.push(path);
-            } else {
-                directories.append(&mut find_directories(path.to_str().unwrap(), criteria));
+    let mut directories: Vec<PathBuf>;
+    let criteria = criteria.split(',').collect::<Vec<&str>>();
+    let mut p = path.to_string();
+    if !p.ends_with('/') {
+        p.push('/');
+    }
+    // https://docs.rs/globwalk/latest/globwalk/
+    directories = criteria
+        .par_iter()
+        .map(|criterion| {
+            let mut path = p.clone();
+            let c = criterion.trim();
+            let c = "**/".to_owned() + c;
+            path.push_str(c.as_str());
+            glob(path.as_str())
+                .expect("Failed to read glob pattern")
+                .filter(|entry| entry.is_ok())
+                .map(|entry| entry.unwrap())
+                .collect::<Vec<PathBuf>>()
+        })
+        .flatten()
+        .collect::<Vec<PathBuf>>();
+
+    // remove duplicates
+    directories.sort();
+    directories.dedup();
+    // keep only the parent directories
+    // know if the path is parent of the current directory
+    let mut is_parent = vec![false; directories.len()];
+    for i in 0..directories.len() {
+        for j in 0..directories.len() {
+            if i == j {
+                continue;
+            }
+            if directories[i].starts_with(&directories[j]) {
+                is_parent[i] = true;
             }
         }
     }
-    directories
+    let mut result = vec![];
+    for i in 0..directories.len() {
+        if !is_parent[i] {
+            result.push(directories[i].clone());
+        }
+    }
+    result
 }
 
 pub fn delete_directory(projects: Vec<PathBuf>) {
